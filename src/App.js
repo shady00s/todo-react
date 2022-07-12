8886e049bb74e5828b9b2e2f09f6edc4bd4e1844
shady00s <ssk2310@hotmail.com>
2022-07-12 16:25:01 +0200

    adding delete and done logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import SignUp from './pages/signup/signup';
-import LogIn from './pages/login/login';
-import Home from './pages/main-page/home';
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap-grid.css'
 import './App.css';
-import { Route,  BrowserRouter as Router, Switch } from 'react-router-dom';
+import Routes from './routes';
+
 
 function App() {
   return (
-    <Router>
-      
-        <Switch>
-          <Route path="/log-in" exact component={LogIn}></Route>
-          <Route path="/sign-up" exact component={SignUp}></Route>
-          <Route path="/home" exact component={Home}></Route>
-        </Switch>
-     
-
-    </Router>
-
+    
+    <Routes/>
 
 
   );
--- src/components/task-body/task.js
+++ src/components/task-body/task.js
@@ -1,44 +1,43 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrashCan,faEdit } from '@fortawesome/free-regular-svg-icons'; 
+import { faTrashCan, faEdit } from '@fortawesome/free-regular-svg-icons';
+import 'bootstrap/dist/css/bootstrap-grid.css'
 import React from 'react';
 import './task.css'
 
 
-export default class TaskBody extends React.Component{
+export default class TaskBody extends React.Component {
 
     constructor(props) {
         super(props);
 
-        this.state ={clickedEvent : true}
+        this.state = { clickedEvent: true }
         this.onclickevent = this.onclickevent.bind(this)
     }
-    
-    onclickevent(){
-        
-            this.setState(clickEvent=>({
-                clickedEvent: !clickEvent.clickedEvent
-            })) 
-        
+
+    onclickevent() {
+        this.props.archivedTask()
+        this.setState(clickEvent => ({
+            clickedEvent: !clickEvent.clickedEvent
+        }))
+
     }
-       
-       
-    render(){
+
+
+    render() {
 
         return <>
-        <div className="taskBody" >
+            <div className="taskBody row" >
+                <div className="taskText col-lg-8 col-sm-12" onClick={this.onclickevent}>
 
-       
-            <div className="taskText" onClick={this.onclickevent}>
-            
-            {this.state.clickedEvent ===true ?<p>Test text</p> :<p> <s>Test text</s> </p> }
-           
+                    {this.state.clickedEvent === true && this.props.element.status ==="pending"? <p >{this.props.element.body}</p> : <p style={{ color: 'grey' }}> <s style={{ color: 'grey' }}>{this.props.element.body}</s> </p>}
+
+                </div>
+                {/* delete or edit button */}
+                <div className="taskButton col-lg-4 ">
+                    <button className='editBtn'><FontAwesomeIcon icon={faEdit}  /> Edit</button>
+                    <button onClick={this.props.deleteFunc } className='deletebtn'><FontAwesomeIcon icon={faTrashCan} /> Delete</button>
+                </div>
             </div>
-            {/* delete or edit button */}
-            <div className="taskButton">
-            <button className='editBtn'><FontAwesomeIcon icon={faEdit}/> Edit</button>
-            <button className='deletebtn'><FontAwesomeIcon icon={faTrashCan}/> Delete</button>
-            </div>
-        </div>
-    </>
-    } 
+        </>
+    }
 }
